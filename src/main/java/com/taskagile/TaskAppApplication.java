3632ce3cf96208406261feadd6d7212b82c5328f
makus18 <makus18@gmail.com>
2021-01-13 20:28:01 +0100

    datasource config

--- src/main/java/com/taskagile/TaskAppApplication.java
+++ src/main/java/com/taskagile/TaskAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskAgileApplication {
+public class TaskAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskAgileApplication.class, args);
+        SpringApplication.run(TaskAppApplication.class, args);
     }
 
 }
